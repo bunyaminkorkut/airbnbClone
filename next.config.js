c6334ed5f2a3056b43a599b293a180f6468772dc
Bünyamin Korkut <bunyaminkrkt@hotmail.com>
2023-01-20 23:37:43 +0300

    deneme

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  experimental: {
-    // Required:
-    appDir: true,
-  },
-};
+  /* config options here */
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
+// const plugins = [withTM];
+
+// module.exports = (_phase, { defaultConfig }) => {
+//   return plugins.reduce(
+//     (acc, plugin) => {
+//       if (Array.isArray(plugin)) {
+//         return plugin[0](acc, plugin[1]);
+//       }
+//       return plugin(acc);
+//     },
+//     { ...nextConfig }
+//   );
+// };
